cbc668f57e1cc19ee8ffedfc47bc6625dcdfe5d0
mtkyjmm <mtkyjmm@outlook.com>
2021-08-03 00:11:02 +0800

    抽出MainTabBar

--- 05-tabbar/src/App.vue
+++ 05-tabbar/src/App.vue
@@ -1,40 +1,18 @@
 <template>
   <div id="app">
-    <tab-bar>
-      <tab-bar-item path='/home' activeColor='green'>
-        <img slot="item-icon" src="./assets/img/tabbar/home.png" alt="" />
-        <img slot="item-icon-active" src="./assets/img/tabbar/home_active.png" alt="" />
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item  path='/call' activeColor='pink'>
-        <img slot="item-icon" src="./assets/img/tabbar/call.png" alt="" />
-        <img slot="item-icon-active" src="./assets/img/tabbar/call_active.png" alt="" />
-        <div slot="item-text">电话</div>
-      </tab-bar-item>
-      <tab-bar-item  path='/message' activeColor='yellow'>
-        <img slot="item-icon" src="./assets/img/tabbar/message.png" alt="" />
-        <img slot="item-icon-active" src="./assets/img/tabbar/message_active.png" alt="" />
-        <div slot="item-text">信息</div>
-      </tab-bar-item>
-      <tab-bar-item  path='/photo'>
-        <img slot="item-icon" src="./assets/img/tabbar/photo.png" alt="" />
-        <img slot="item-icon-active" src="./assets/img/tabbar/photo_active.png" alt="" />
-        <div slot="item-text">照片</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
     <router-view />
   </div>
 </template>
 
 <script>
-import TabBar from "./components/tabbar/Tabbar";
-import TabBarItem from "./components/tabbar/TabBarItem";
-// import TabBarItem from './components/tabbar/TabBarItem.vue'
+
+import MainTabBar from './components/tabbar/MainTabBar'
+
 export default {
   name: "App",
   components: {
-    TabBar,
-    TabBarItem,
+    MainTabBar
   },
 };
 </script>
---
+++ 05-tabbar/src/components/tabbar/MainTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="main-tab-bar">
+    <tab-bar>
+      <tab-bar-item path='/home' activeColor='green'>
+        <img slot="item-icon" src="../../assets/img/tabbar/home.png" alt="" />
+        <img slot="item-icon-active" src="../../assets/img/tabbar/home_active.png" alt="" />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item  path='/call' activeColor='pink'>
+        <img slot="item-icon" src="../../assets/img/tabbar/call.png" alt="" />
+        <img slot="item-icon-active" src="../../assets/img/tabbar/call_active.png" alt="" />
+        <div slot="item-text">电话</div>
+      </tab-bar-item>
+      <tab-bar-item  path='/message' activeColor='deeppink'>
+        <img slot="item-icon" src="../../assets/img/tabbar/message.png" alt="" />
+        <img slot="item-icon-active" src="../../assets/img/tabbar/message_active.png" alt="" />
+        <div slot="item-text">信息</div>
+      </tab-bar-item>
+      <tab-bar-item  path='/photo'>
+        <img slot="item-icon" src="../../assets/img/tabbar/photo.png" alt="" />
+        <img slot="item-icon-active" src="../../assets/img/tabbar/photo_active.png" alt="" />
+        <div slot="item-text">照片</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "./Tabbar";
+import TabBarItem from "./TabBarItem";
+export default {
+  name: "MainTabBar",
+   components: {
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
